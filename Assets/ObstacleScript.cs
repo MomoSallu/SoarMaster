1bd76b0a9e52c0c8899e7800da6380136ea0b482
MomoSallu <muhammadsallu@gmail.com>
2023-10-22 13:59:35 +0100

    Changed obstacle sprite, creates and destroys obstacles properly
    Changed the sprite used for obstacles, obstacles spawn of the right and move to the left, destroys once it's passed a certain x position.

--- Assets/ObstacleScript.cs
+++ Assets/ObstacleScript.cs
@@ -7,15 +7,22 @@ public class ObstacleScript : MonoBehaviour
     // Start is called before the first frame update
 
     public float moveSpeed = 2;
+    public float offScreenZone = -5;
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime; //Makes sure that the multplication doesn't factor the fps
+
+        if (transform.position.x < offScreenZone)
+        {
+            Debug.Log("Obstacle deleted");
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/ObstacleSpawnScript.cs
+++ Assets/ObstacleSpawnScript.cs
@@ -7,21 +7,31 @@ public class ObstacleSpawnScript : MonoBehaviour
     // Start is called before the first frame update
 
     public GameObject obstacle;
-    public float obstacleSpawnTimer = 3;
+    public float obstacleSpawnTimer = 2;
     private float timer = 0;
+    public float heightOffset = 10f;
 
     void Start()
     {
-        
+        spawnObstacle();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(timer < obstacleSpawnTimer)
+        if (timer < obstacleSpawnTimer)
         {
             timer += Time.deltaTime;
-        } else
-        Instantiate(obstacle, transform.position, transform.rotation);
+        }
+        else
+        {
+            spawnObstacle();
+            timer = 0;
+        }
+    }
+    void spawnObstacle() {
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+        Instantiate(obstacle, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
     }
 }
