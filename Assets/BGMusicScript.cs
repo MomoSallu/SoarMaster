2e646a54d6fe7ecd6d58c2dad0a04034b479d842
MomoSallu <muhammadsallu@gmail.com>
2023-10-24 19:48:19 +0100

    Added Background Music
    Added an object that doesn't get destroyed when moving scenes so it continually plays from the menu to the actual game.

---
+++ Assets/BGMusicScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMusicScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource bg;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
